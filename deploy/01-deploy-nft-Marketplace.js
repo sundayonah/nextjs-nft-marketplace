d8a38edc1bf597ba5d0c28e34d77e0e1ff9d308b
sundayonah <sundayonah94@gmail.com>
2023-05-08 14:55:02 +0100

    update on deploy

--- deploy/01-deploy-nft-Marketplace.js
+++ deploy/01-deploy-nft-Marketplace.js
@@ -8,41 +8,35 @@ const { verify } = require("../utils/verify")
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
-    let vrfCoordinatorV2Address, subscriptionId
-    const chainId = network.config.chainId
-    if (developmentChains.includes(network.name)) {
-        const vrfCoordinatorV2Mock = await ethers.getContract(
-            "VRFCoordinatorV2Mock"
-        )
-        vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
-        const transactionResponse =
-            await vrfCoordinatorV2Mock.createSubscription()
-        const transactionReceipt = await transactionResponse.wait(1)
-        subscriptionId = transactionReceipt.events[0].args.subId
-        //Fund the subscription
-        //Usually, you'd need the link token on a real network
-        await vrfCoordinatorV2Mock.fundSubscription(
-            subscriptionId,
-            VRF_SUB_FUND_AMOUNT
-        )
-    } else {
-        vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"]
-        subscriptionId = networkConfig[chainId]["subscriptionId"]
-    }
-    const entranceFee = networkConfig[chainId]["entranceFee"]
-    const gasLane = networkConfig[chainId]["gasLane"]
-    callbackGasLimit = networkConfig[chainId]["callbackGasLimit"]
-    interval = networkConfig[chainId]["interval"]
+    // let vrfCoordinatorV2Address, subscriptionId
+    // const chainId = network.config.chainId
+    // if (developmentChains.includes(network.name)) {
+    //     const vrfCoordinatorV2Mock = await ethers.getContract(
+    //         "VRFCoordinatorV2Mock"
+    //     )
+    //     vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+    //     const transactionResponse =
+    //         await vrfCoordinatorV2Mock.createSubscription()
+    //     const transactionReceipt = await transactionResponse.wait(1)
+    //     subscriptionId = transactionReceipt.events[0].args.subId
+    //     //Fund the subscription
+    //     //Usually, you'd need the link token on a real network
+    //     await vrfCoordinatorV2Mock.fundSubscription(
+    //         subscriptionId,
+    //         VRF_SUB_FUND_AMOUNT
+    //     )
+    // } else {
+    //     vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"]
+    //     subscriptionId = networkConfig[chainId]["subscriptionId"]
+    // }
+    // const entranceFee = networkConfig[chainId]["entranceFee"]
+    // const gasLane = networkConfig[chainId]["gasLane"]
+    // callbackGasLimit = networkConfig[chainId]["callbackGasLimit"]
+    // interval = networkConfig[chainId]["interval"]
 
-    const args = [
-        vrfCoordinatorV2Address,
-        entranceFee,
-        gasLane,
-        subscriptionId,
-        callbackGasLimit,
-        interval,
-    ]
-    const raffle = await deploy("Raffle", {
+    const args = []
+
+    const nftMarketplace = await deploy("NftMarketplace", {
         from: deployer,
         args: args,
         log: true,
@@ -54,9 +48,9 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
         process.env.ETHERSCAN_API_KEY
     ) {
         log("Verifying.......")
-        await verify(raffle.address, args)
+        await verify(nftMarketplace.address, args)
     }
     log("----------------last-----------------")
 }
 
-module.exports.tags = ["all", "raffle"]
+module.exports.tags = ["all", "nftMarketplace"]
